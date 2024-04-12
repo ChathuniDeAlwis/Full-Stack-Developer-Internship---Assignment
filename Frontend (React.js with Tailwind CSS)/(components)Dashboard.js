0cee11741b5c1b7d00183147371f35561e1a3413
Chathuni De Alwis <138837563+ChathuniDeAlwis@users.noreply.github.com>
2024-04-12 19:40:20 +0530

    Create (components)Dashboard.js

---
+++ Frontend (React.js with Tailwind CSS)/(components)Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentList from './StudentList';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <StudentList />
+    </div>
+  );
+};
+
+export default Dashboard;
